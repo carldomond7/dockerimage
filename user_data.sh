ab0340200bd6bd452036d7e84c096ffa06c6ef05
carldomond7 <124755395+carldomond7@users.noreply.github.com>
2023-09-30 14:00:11 -0700

    Updating script to not only run docker run command, but to first install docker, start the service, then run the image from dockerhub

--- user_data.sh
+++ user_data.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
-docker run -d starseizer45/packerprojects:latest
+#Install Docker, first update apt package manager
+sudo apt-get -y update
+sudo apt-get install -y docker.io
+
+#Start Docker Service
+sudo systemctl start docker
+sudo systemctl enable docker 
+
+#Run my docker container 
+sudo docker run -d starseizer45/packerprojects:latest
